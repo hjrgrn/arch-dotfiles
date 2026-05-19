c319dc095618a2f9d81848c992ae9990c3525d70
hjrgrn <187955624+hjrgrn@users.noreply.github.com>
2026-05-19 16:10:50 +0200

    setup: Improve setup script

--- setup.sh
+++ setup.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env sh
 
+sudo pacman -S hyprland alacritty noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ttf-mononoki-nerd mako vlc pipewire wireplumber pipewire-audio pipewire-pulse pipewire-alsa pipewire-jack pavucontrol xdg-desktop-portal-hyprland hyprpolkitagent qt5-wayland qt6-wayland nemo imv mpv
 
-dir=$HOME/.config/hypr
+readonly config="$HOME/.config"
+dir="${config}/hypr"
 
-if [ -d $dir ]; then
-    rm -r $dir
+if [ -d "$dir" ]; then
+    rm -r "$dir"
 fi
 
-cp -r hypr/ $dir
+cp -r hypr "$config"
 
-dir=$HOME/.config/waybar
+dir="${config}/waybar"
 
-if [ -d $dir ]; then
-    rm -r $dir
+if [ -d "$dir" ]; then
+    rm -r "$dir"
 fi
 
-cp -r waybar $dir
+cp -r waybar "$config"
 
 echo "Hyprland has been configured."
